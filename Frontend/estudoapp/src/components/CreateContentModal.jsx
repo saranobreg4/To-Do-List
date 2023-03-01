5fd580d9ae92fd654fd8c335b60321276cdca988
Edlayne Oliveira <114404910+EdlayneOliveiraa@users.noreply.github.com>
2023-03-01 12:55:08 -0300

    Add files via upload

---
+++ Frontend/estudoapp/src/components/CreateContentModal.jsx
@@ -0,0 +1,49 @@
+import { Modal, Button, Form } from 'react-bootstrap'
+function CreateContentModal(props) {
+  return(
+    <div
+      className="modal show"
+      style={{ display: 'block', position: 'initial' }}
+    >
+      <Modal show={props.isModalOpen}>
+        <Form onSubmit={(event) => {
+          props.createContent(event)
+        }}>
+        <Modal.Header closeButton onClick={props.handleClose}>
+          <Modal.Title>Crie sua lista</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <Form.Group controlId="titulo">
+            <Form.Label>
+              Afazeres
+            </Form.Label>
+            <Form.Control type="text" />
+          </Form.Group>
+
+          <Form.Group controlId="descricao">
+            <Form.Label>
+              Descrição/ Detalhes
+            </Form.Label>
+            <Form.Control type="text" />
+          </Form.Group>
+
+          <Form.Group controlId="porcentagem">
+            <Form.Label>
+              Prioridade (0 a 10)
+            </Form.Label>
+            <Form.Control type="number" />
+          </Form.Group>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <Button variant="secondary" onClick={props.handleClose}>Fechar</Button>
+          <Button variant="primary" type="submit">Salvar</Button>
+        </Modal.Footer>
+        </Form>
+      </Modal >
+    </div>
+  )
+}
+
+export default CreateContentModal
--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# To-Do-List
+# Projeto-de-Aula-Modulo-5
+Case 1 Meu app
+
+O tema do projeto é livre, você pode pensar que tipo de cadastro seria
+interessante e se tornaria um produto legal:
+Que tal uma lista de livros que foram lidos? Ou então uma lista de filmes para
+assistir este ano? Ou quem sabe quais são os games que o usuário já jogou e
+terminou … são muitas possibilidades de listas!
+
+
+### 1. Instalar as dependências
+
+```bash
+cd estudoapp
+npm install react-bootstrap bootstrap
+npm install react-router-dom
+```
+
+## 2. Como executar?
+```bash
+npm install
+npm run dev
+```
+
+⇨ LISTA PREENCHIDA:
+
+<img>![lista preenchida](https://user-images.githubusercontent.com/114404910/221890413-df666916-4586-4a5f-88ad-c0363b54e077.png)</img>
